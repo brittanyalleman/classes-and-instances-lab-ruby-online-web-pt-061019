15a423a0409c31cc8fa7cc7611a5f9876a6598d0
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-11 20:07:12 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,25 @@
-class person
+class Person
 
 def initialize(name)
   @name = name
-end 
+end
+
+def name
+ return @name
+ end
+
+ def name= (name)
+ @name = name
+ end
+
+end
+
+end
+
+adele_goldberg = Person.new(adele_goldberg)
+adele_goldberg.name = adele_goldberg
+puts adele_goldberg.name
+
+alan_kay = Person.new(alan_kay)
+alan_kay.name = alan_kay
+puts alan_kay.name 
